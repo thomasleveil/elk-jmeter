e264c409b2ae8cd3a101b9b689d98c6d80592374
Thomas LÉVEIL <thomasleveil@gmail.com>
2015-06-17 00:39:52 +0000

    first commit

---
+++ logstash/perfmon.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python -u
+
+# telnet program example
+import socket, select, string, sys, os
+ 
+#main function
+if __name__ == "__main__":
+     
+    if(len(sys.argv) < 5) :
+        print 'Usage : %s hostname port interval metrics' % os.path.basename(sys.argv[0])
+        sys.exit(1)
+    
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+     
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print 'Unable to connect'
+        sys.exit(2)
+
+    s.send("interval:%s\n" % sys.argv[3])
+    s.send("metrics:%s\n" % sys.argv[4])
+
+    try:
+        while 1:
+            socket_list = [sys.stdin, s]
+             
+            # Get the list sockets which are readable
+            read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+             
+            for sock in read_sockets:
+                #incoming message from remote server
+                if sock == s:
+                    data = sock.recv(4096)
+                    if not data :
+                        print 'Connection closed'
+                        sys.exit()
+                    else :
+                        #print data
+                        sys.stdout.write(data)
+                 
+                #user entered a message
+                else :
+                    msg = sys.stdin.readline()
+                    s.send(msg)
+    except KeyboardInterrupt:
+        pass
